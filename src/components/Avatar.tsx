75126eb29f6933e40c816e52ebf3de07a09d6e6b
RadoKolev <rado.k.kolev@gmail.com>
2024-08-18 11:57:00 +0100

    Add components

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import { FC } from 'react';
+
+
+type Props = {
+  img: string;
+  alt?: string;
+};
+
+const Avatar: FC<Props> = ({ img, alt }) => {
+  return (
+    <div className='rounded-full overflow-hidden w-full pt-[100%] relative'>
+      <div className='absolute inset-0'>
+        <Image src={img} alt={alt || img} />
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
